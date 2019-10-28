05100d81e0f7ed0f7feecbd483b234c7ce436bed
Leandro Cardozo <lcardozo@silentiumapps.com>
2019-10-28 14:43:57 -0300

    Add file

--- Ranking code.py
+++ Ranking code.py
@@ -179,6 +179,47 @@ for item in ArrayFlair:
         row += 1
 workbook.close()
 
+workbook = xlsxwriter.Workbook('Statistics.xlsx') 
+worksheet = workbook.add_worksheet() 
+row = 0
+column = 0
+
+worksheet.write(row, column, "Statistics")
+worksheet.write(row+1, column, "Total Post Upvotes")
+worksheet.write(row+1, column+1, TotalPostUpvotes)
+worksheet.write(row+2, column, "Total Post Downvotes")
+worksheet.write(row+2, column+1, TotalPostDownvotes)
+worksheet.write(row+3, column, "Total Post Average Upvotes")
+worksheet.write(row+3, column+1, round(TotalPostUpvotes/PostQuantity,2))
+worksheet.write(row+4, column, "Total Post Average Downvote")
+worksheet.write(row+4, column+1, round(TotalPostDownvotes/PostQuantity,2))
+worksheet.write(row+5, column, "Comment Quantity")
+worksheet.write(row+5, column+1, CommentQuantity)
+worksheet.write(row+6, column, "Post Quantity")
+worksheet.write(row+6, column+1, PostQuantity)
+worksheet.write(row+7, column, "Total Comment Upvotes")
+worksheet.write(row+7, column+1, TotalCommentUpvotes)
+worksheet.write(row+8, column, "Average Upvotes Comment")
+worksheet.write(row+8, column+1, round(TotalCommentUpvotes/PostQuantity,2))
+worksheet.write(row+9, column, "The worst comment is")
+worksheet.write(row+9, column+1, WorstComment.translate(non_bmp_map))
+worksheet.write(row+10, column, "With")
+worksheet.write(row+10, column+1, WorstCommentUpvote)
+worksheet.write(row+11, column, "upvotes of the author")
+worksheet.write(row+11, column+1, WorstAuthor)
+worksheet.write(row+12, column, "of the post")
+worksheet.write(row+12, column+1, WorstCommentPost.translate(non_bmp_map))
+worksheet.write(row+13, column, "The best comment is")
+worksheet.write(row+13, column+1, BestComment.translate(non_bmp_map))
+worksheet.write(row+14, column, "With")
+worksheet.write(row+14, column+1, BestCommentUpvote)
+worksheet.write(row+15, column, "upvotes of the author")
+worksheet.write(row+15, column+1, BestAuthor)
+worksheet.write(row+16, column, "of the post")
+worksheet.write(row+16, column+1, BestCommentPost.translate(non_bmp_map))
+
+workbook.close()
+
 try:
     print('Total Post Upvotes: '+str(TotalPostUpvotes))
     print('Total Post Downvotes: '+str(TotalPostDownvotes))
