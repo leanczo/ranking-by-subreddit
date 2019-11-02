8fab38b188802b50e9c4e0f8b14268ef71237416
Leandro Cardozo <lcardozo@silentiumapps.com>
2019-11-02 01:03:14 -0300

    .

--- Ranking code.py
+++ Ranking code.py
@@ -106,103 +106,105 @@ for submission in new_python:
     except UnicodeEncodeError:
         print('Unicode Encode Error')
 
-workbook = xlsxwriter.Workbook('Posts.xlsx') 
-worksheet = workbook.add_worksheet() 
-row = 0
-column = 0
-worksheet.write(row, column, "N°")
-worksheet.write(row, column+1, "User")
-worksheet.write(row, column+2, "Upvotes")
-worksheet.write(row, column+3, "Downvotes Posts")
-worksheet.write(row, column+4, "Quantity Posts")
+def GenerateDocuments():
+    workbook = xlsxwriter.Workbook('Posts.xlsx') 
+    worksheet = workbook.add_worksheet() 
+    row = 0
+    column = 0
+    worksheet.write(row, column, "N°")
+    worksheet.write(row, column+1, "User")
+    worksheet.write(row, column+2, "Upvotes")
+    worksheet.write(row, column+3, "Downvotes Posts")
+    worksheet.write(row, column+4, "Quantity Posts")
 
-for item in ArrayPost: 
-    worksheet.write(row+1, column+1, item)
-    worksheet.write(row+1, 0, row+1)
-    column += 1
-    if (column% 4) == 0:
-        column = 0
-        row += 1
-workbook.close() 
+    for item in ArrayPost: 
+        worksheet.write(row+1, column+1, item)
+        worksheet.write(row+1, 0, row+1)
+        column += 1
+        if (column% 4) == 0:
+            column = 0
+            row += 1
+    workbook.close() 
 
-workbook = xlsxwriter.Workbook('Comments.xlsx') 
-worksheet = workbook.add_worksheet()
-row = 0
-column = 0
+    workbook = xlsxwriter.Workbook('Comments.xlsx') 
+    worksheet = workbook.add_worksheet()
+    row = 0
+    column = 0
 
-worksheet.write(row, column, "N°")
-worksheet.write(row, column+1, "User")
-worksheet.write(row, column+2, "Upvotes")
-worksheet.write(row, column+3, "Quantity Comments")
+    worksheet.write(row, column, "N°")
+    worksheet.write(row, column+1, "User")
+    worksheet.write(row, column+2, "Upvotes")
+    worksheet.write(row, column+3, "Quantity Comments")
 
-for item in ArrayCommnent: 
-    worksheet.write(row+1, column+1, item)
-    worksheet.write(row+1, 0, row+1)
-    column += 1
-    if (column  % 3) == 0:
-        column = 0
-        row += 1
-workbook.close()
+    for item in ArrayCommnent: 
+        worksheet.write(row+1, column+1, item)
+        worksheet.write(row+1, 0, row+1)
+        column += 1
+        if (column  % 3) == 0:
+            column = 0
+            row += 1
+    workbook.close()
 
-workbook = xlsxwriter.Workbook('Flairs.xlsx') 
-worksheet = workbook.add_worksheet() 
-row = 0
-column = 0
+    workbook = xlsxwriter.Workbook('Flairs.xlsx') 
+    worksheet = workbook.add_worksheet() 
+    row = 0
+    column = 0
 
-worksheet.write(row, column, "N°")
-worksheet.write(row, column+1, "Flairs")
-worksheet.write(row, column+2, "Quantity")
+    worksheet.write(row, column, "N°")
+    worksheet.write(row, column+1, "Flairs")
+    worksheet.write(row, column+2, "Quantity")
 
-for item in ArrayFlair: 
-    worksheet.write(row+1, column+1, item)
-    worksheet.write(row+1, 0, row+1)
-    column += 1
-    if (column  % 2) == 0:
-        column = 0
-        row += 1
-workbook.close()
+    for item in ArrayFlair: 
+        worksheet.write(row+1, column+1, item)
+        worksheet.write(row+1, 0, row+1)
+        column += 1
+        if (column  % 2) == 0:
+            column = 0
+            row += 1
+    workbook.close()
 
-workbook = xlsxwriter.Workbook('Statistics.xlsx') 
-worksheet = workbook.add_worksheet() 
-row = 0
-column = 0
+    workbook = xlsxwriter.Workbook('Statistics.xlsx') 
+    worksheet = workbook.add_worksheet() 
+    row = 0
+    column = 0
 
-worksheet.write(row, column, "Statistics")
-worksheet.write(row, column+1, "Quantity")
-worksheet.write(row+1, column, "Total Post Upvotes")
-worksheet.write(row+1, column+1, TotalPostUpvotes)
-worksheet.write(row+2, column, "Total Post Downvotes")
-worksheet.write(row+2, column+1, TotalPostDownvotes)
-worksheet.write(row+3, column, "Total Post Average Upvotes")
-worksheet.write(row+3, column+1, round(TotalPostUpvotes/PostQuantity,2))
-worksheet.write(row+4, column, "Total Post Average Downvote")
-worksheet.write(row+4, column+1, round(TotalPostDownvotes/PostQuantity,2))
-worksheet.write(row+5, column, "Comment Quantity")
-worksheet.write(row+5, column+1, CommentQuantity)
-worksheet.write(row+6, column, "Post Quantity")
-worksheet.write(row+6, column+1, PostQuantity)
-worksheet.write(row+7, column, "Total Comment Upvotes")
-worksheet.write(row+7, column+1, TotalCommentUpvotes)
-worksheet.write(row+8, column, "Average Upvotes Comment")
-worksheet.write(row+8, column+1, round(TotalCommentUpvotes/PostQuantity,2))
-worksheet.write(row+9, column, "The worst comment is")
-worksheet.write(row+9, column+1, WorstComment.translate(non_bmp_map))
-worksheet.write(row+10, column, "With")
-worksheet.write(row+10, column+1, WorstCommentUpvote)
-worksheet.write(row+11, column, "upvotes of the author")
-worksheet.write(row+11, column+1, WorstAuthor)
-worksheet.write(row+12, column, "of the post")
-worksheet.write(row+12, column+1, WorstCommentPost.translate(non_bmp_map))
-worksheet.write(row+13, column, "The best comment is")
-worksheet.write(row+13, column+1, BestComment.translate(non_bmp_map))
-worksheet.write(row+14, column, "With")
-worksheet.write(row+14, column+1, BestCommentUpvote)
-worksheet.write(row+15, column, "upvotes of the author")
-worksheet.write(row+15, column+1, BestAuthor)
-worksheet.write(row+16, column, "of the post")
-worksheet.write(row+16, column+1, BestCommentPost.translate(non_bmp_map))
-workbook.close()
+    worksheet.write(row, column, "Statistics")
+    worksheet.write(row, column+1, "Quantity")
+    worksheet.write(row+1, column, "Total Post Upvotes")
+    worksheet.write(row+1, column+1, TotalPostUpvotes)
+    worksheet.write(row+2, column, "Total Post Downvotes")
+    worksheet.write(row+2, column+1, TotalPostDownvotes)
+    worksheet.write(row+3, column, "Total Post Average Upvotes")
+    worksheet.write(row+3, column+1, round(TotalPostUpvotes/PostQuantity,2))
+    worksheet.write(row+4, column, "Total Post Average Downvote")
+    worksheet.write(row+4, column+1, round(TotalPostDownvotes/PostQuantity,2))
+    worksheet.write(row+5, column, "Comment Quantity")
+    worksheet.write(row+5, column+1, CommentQuantity)
+    worksheet.write(row+6, column, "Post Quantity")
+    worksheet.write(row+6, column+1, PostQuantity)
+    worksheet.write(row+7, column, "Total Comment Upvotes")
+    worksheet.write(row+7, column+1, TotalCommentUpvotes)
+    worksheet.write(row+8, column, "Average Upvotes Comment")
+    worksheet.write(row+8, column+1, round(TotalCommentUpvotes/PostQuantity,2))
+    worksheet.write(row+9, column, "The worst comment is")
+    worksheet.write(row+9, column+1, WorstComment.translate(non_bmp_map))
+    worksheet.write(row+10, column, "With")
+    worksheet.write(row+10, column+1, WorstCommentUpvote)
+    worksheet.write(row+11, column, "upvotes of the author")
+    worksheet.write(row+11, column+1, WorstAuthor)
+    worksheet.write(row+12, column, "of the post")
+    worksheet.write(row+12, column+1, WorstCommentPost.translate(non_bmp_map))
+    worksheet.write(row+13, column, "The best comment is")
+    worksheet.write(row+13, column+1, BestComment.translate(non_bmp_map))
+    worksheet.write(row+14, column, "With")
+    worksheet.write(row+14, column+1, BestCommentUpvote)
+    worksheet.write(row+15, column, "upvotes of the author")
+    worksheet.write(row+15, column+1, BestAuthor)
+    worksheet.write(row+16, column, "of the post")
+    worksheet.write(row+16, column+1, BestCommentPost.translate(non_bmp_map))
+    workbook.close()
 
+GenerateDocs()
 FinalTime = time() 
 ExecutionTime = FinalTime  - InitialTime
 print ('Execution Time:',round(ExecutionTime ,2),'seconds ó',round(ExecutionTime/60,2),'minutes') 
